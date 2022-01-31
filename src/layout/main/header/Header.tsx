2aa0cf653b55503090cd156c4db4a00af1677129
AvanesyanAshot <9teron9@mail.ru>
2022-01-31 19:45:04 +0300

    feature/add scroll header

--- src/layout/main/header/Header.tsx
+++ src/layout/main/header/Header.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import css from "./Header.module.css";
 import { ReactComponent as Theme } from "../../../assets/svg/moon.svg";
+import clsx from "clsx";
 
 const Header = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => setScroll(window.scrollY > 40));
+    return () => {
+      window.removeEventListener("scroll", () =>
+        setScroll(window.scrollY > 40)
+      );
+    };
+  }, []);
+
   return (
-    <header className={css.header}>
+    <header className={clsx(css.header, { [css.scroll__header]: scroll })}>
       <nav className="container">
         <Link className={css.nav__logo} to="/">
           Ashot
