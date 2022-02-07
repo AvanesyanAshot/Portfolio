6143834c903405e8d34cfa7e1dcb58c26fcc85c0
AvanesyanAshot <9teron9@gmail.com>
2022-02-07 12:22:22 +0300

    feature/add footer

--- src/layout/main/footer/Footer.tsx
+++ src/layout/main/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import React from "react";
 import css from "./Footer.module.css";
+import { ReactComponent as WhatsApp } from "../../../assets/svg/whatsapp.svg";
+import { ReactComponent as Telegram } from "../../../assets/svg/telegram.svg";
 const Footer = () => {
   return (
     <footer className={css.footer}>
@@ -8,7 +10,9 @@ const Footer = () => {
         <h1 className={css.title}>Ashot</h1>
         <ul className={css.list}>
           <li>
-            <a href="#about">About</a>
+            <a href="#about" className={css.link}>
+              About
+            </a>
           </li>
           <li>
             <a href="#work">Work</a>
@@ -16,20 +20,20 @@ const Footer = () => {
         </ul>
         <ul className={css.social}>
           <li>
-            <a className={css.button} href="https://t.me/AvanesyanAshot">
-              Telegram
+            <a className={css.social__link} href="https://t.me/AvanesyanAshot">
+              <Telegram />
             </a>
           </li>
           <li>
             <a
-              className={css.button}
+              className={css.social__link}
               href="https://api.whatsapp.com/send?phone=79629492870&text=Hello"
             >
-              Whatsapp
+              <WhatsApp />
             </a>
           </li>
         </ul>
-        <span>&#169; Ashot.All rights reserved</span>
+        <span className={css.copy}>&#169; Ashot.All rights reserved</span>
       </div>
     </footer>
   );
