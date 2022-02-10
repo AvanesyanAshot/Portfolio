b898a30e6db561bc9aa722101ecb26f4d8bc33ed
AvanesyanAshot <9teron9@gmail.com>
2022-02-10 11:36:02 +0300

    fix/remove EventListener

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,29 @@ import Home from "./components/home/Home";
 import Skills from "./components/skills/Skills";
 import Work from "./components/work/Work";
 const App = () => {
+  const scrollActive = (sections: NodeListOf<Element>) => {
+    const scrollY = window.pageYOffset;
+    sections.forEach((el: any) => {
+      const sectionHeight = el.offsetHeight,
+        sectionTop = el.offsetTop - 70,
+        sectionId = el.getAttribute("id");
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector(`nav a[href*=${sectionId}]`)
+          ?.classList.add("active__link");
+      } else {
+        document
+          .querySelector(`nav a[href*=${sectionId}]`)
+          ?.classList.remove("active__link");
+      }
+    });
+  };
   useEffect(() => {
     const sections = document.querySelectorAll("section[id]");
-    window.addEventListener("scroll", () => {
-      const scrollY = window.pageYOffset;
-      sections.forEach((el: any) => {
-        const sectionHeight = el.offsetHeight,
-          sectionTop = el.offsetTop - 70,
-          sectionId = el.getAttribute("id");
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-          document
-            .querySelector(`nav a[href*=${sectionId}]`)
-            ?.classList.add("active__link");
-        } else {
-          document
-            .querySelector(`nav a[href*=${sectionId}]`)
-            ?.classList.remove("active__link");
-        }
-      });
-    });
+    window.addEventListener("scroll", () => scrollActive(sections));
+    return () => {
+      window.removeEventListener("scroll", () => scrollActive(sections));
+    };
   }, []);
 
   return (
