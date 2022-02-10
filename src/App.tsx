53607246c48f21f725f36ea04f7ba028ab3f8be9
AvanesyanAshot <9teron9@gmail.com>
2022-02-10 11:31:42 +0300

    fix/fix prev commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,9 @@ import React, { useEffect } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Home from "./components/home/Home";
-import NavMenu from "./components/nav-menu/NavMenu";
 import Skills from "./components/skills/Skills";
 import Work from "./components/work/Work";
-import { useNavigate } from "react-router-dom";
 const App = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     const sections = document.querySelectorAll("section[id]");
     window.addEventListener("scroll", () => {
